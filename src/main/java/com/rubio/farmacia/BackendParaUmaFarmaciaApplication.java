90a22ecc9be3c82daa2fa5cc77b136fb6bd19c7f
Rubio <rubiodainton@gmail.com>
2024-12-16 09:59:53 -0300

    Primeira etapa concluída-Configuração

---
+++ src/main/java/com/rubio/farmacia/BackendParaUmaFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.rubio.farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendParaUmaFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendParaUmaFarmaciaApplication.class, args);
+	}
+
+}
